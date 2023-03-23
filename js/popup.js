f0bda8bee6de96cf917ff1e67bd074e80a6ec53c
chrischen0405 <chrischen0405@gmail.com>
2023-03-23 15:37:28 +0800

    feat: 切换隐藏显示自动刷新页面

--- js/popup.js
+++ js/popup.js
@@ -9,7 +9,9 @@ window.onload = function () {
                 target: {
                     tabId
                 },
-                func: clearTitle
+                func: function () {
+                    document.title='新标签页'
+                }
             })
         })
     })
@@ -30,11 +32,13 @@ window.onload = function () {
         isShowHeader = !isShowHeader
         isShowHeaderBtn.innerText = `${isShowHeader ? '头部隐藏' : '头部显示'}`
         chrome.storage.local.set({ isShowHeader })
+        chrome.tabs.reload()
     })
     isShowSidebarBtn.addEventListener('click', () => {
         isShowSidebar = !isShowSidebar
         isShowSidebarBtn.innerText = `${isShowSidebar ? '侧边栏隐藏' : '侧边栏显示'}`
         chrome.storage.local.set({ isShowSidebar })
+        chrome.tabs.reload()
     })
 }
 
@@ -47,9 +51,4 @@ function getCurrentTabId(callback) {
     }, function(tabs) {
         if(callback) callback(tabs.length ? tabs[0].id : null)
     })
-}
-
-// 清空标题
-function clearTitle () {
-    document.title='新标签页'
 }
